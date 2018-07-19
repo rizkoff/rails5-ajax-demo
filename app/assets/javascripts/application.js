1f0cd8577b37722affc68f68494b60a01c5d195f
Andrey Ryzhkov <rizkoff@gmail.com>
2018-07-19 16:32:23 +0300

    post scaffold + application.js :onerror

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).on("ajax:error", function(xhr, status, error) {
+  console.log(status.responseText);
+  return console.log(error);
+});
